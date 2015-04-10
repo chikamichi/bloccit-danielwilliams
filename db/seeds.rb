d92e147ecfad2ba06938e17afe112b713ba4ac08
Daniel Williams <danwilliams7227@gmail.com>
2015-04-10 17:01:02 +0000

    Updated Seed data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,32 @@ require 'faker'
         body:  Faker::Lorem.paragraph
         )
 end
-posts = Post.all
+# posts = Post.all
+post_count = 50
+if Post.exists?(title: "This is a test string")
+else
+  Post.create!(
+      title: "This is a test string",
+      body:  "Making progress"
+      )
+  post_count = post_count + 1
+end
+    
+    
 
 # Create Comments
 100.times do
     Comment.create!(
-        post: posts.sample,
+        post: Post.all.sample,
         body: Faker::Lorem.paragraph
         )
 end
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
+# puts "51 new posts created"
+puts "#{post_count} new posts created"
+puts "100 new comments"
+
+
